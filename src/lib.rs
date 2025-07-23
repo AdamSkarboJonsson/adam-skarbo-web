7a24134930b62e4f740086bb9d355c0dd8d78cf6
Adam Skärbo Jonsson <adskarbo@gmail.com>
2025-07-23 21:09:59 +0200

    routes

--- src/lib.rs
+++ src/lib.rs
@@ -28,6 +28,7 @@ pub fn App() -> impl IntoView {
         <Router>
             <Routes fallback=|| view! { <NotFound /> }>
                 <Route path=path!("/") view=Home/>
+                <Route path=path!("/adam-skarbo-web/") view=Home/>
             </Routes>
         </Router>
     }
